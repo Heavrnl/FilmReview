fc8507601d373576ff786e8e65db87106ff33560
Heavrnl <su@tuyu.me>
2023-09-16 10:42:10 +0800

    添加项目文件。

---
+++ FilmReview/Models/Film.cs
@@ -0,0 +1,16 @@
+﻿namespace FilmReview.Models
+{
+    public class Film
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Director { get; set; }
+
+        public int Rating { get; set; }
+
+        public int VotesNumber { get; set; }
+        public string[]? Actors   { get; set; }
+        public DateTime PublicationDate { get; set; }
+    }
+}
---
+++ FilmReview/Models/Review.cs
@@ -0,0 +1,8 @@
+﻿namespace FilmReview.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+    }
+}
